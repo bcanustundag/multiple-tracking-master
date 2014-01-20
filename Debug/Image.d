b676e49dc2c5f6dc6780bbf6b3f549c33d8ed292
bcanustundag <bcanustundag@gmail.com>
2014-01-20 02:26:49 +0200

    ciao debug

--- Debug/Image.d
+++
@@ -1,8 +0,0 @@
-Image.d: ../Image.cpp ../Image.hpp /usr/local/include/opencv/cv.h \
- /usr/local/include/opencv/cxcore.h
-
-../Image.hpp:
-
-/usr/local/include/opencv/cv.h:
-
-/usr/local/include/opencv/cxcore.h:
